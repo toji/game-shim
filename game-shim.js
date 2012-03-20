7d95c427716f510a7ebe1f31bd409e4348ebc458
Brandon Jones <txmg64@motorola.com>
2012-03-20 09:04:16 -0700

    Normalized code changes from raphaelr to match the style of the rest of the code.

--- game-shim.js
+++ game-shim.js
@@ -49,22 +49,22 @@
     // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
     (function() {
         var lastTime = 0;
-        var vendors = ['webkit', 'moz', 'ms', 'o'];
+        var vendors = ["webkit", "moz", "ms", "o"];
         var x;
 
         for(x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+            window.requestAnimationFrame = window[vendors[x]+"RequestAnimationFrame"];
         }
 
         window.cancelAnimationFrame = window.cancelAnimationFrame || window.cancelRequestAnimationFrame; // Check for older syntax
         for(x = 0; x < vendors.length && !window.cancelAnimationFrame; ++x) {
-            window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+            window.cancelAnimationFrame = window[vendors[x]+"CancelAnimationFrame"] || window[vendors[x]+"CancelRequestAnimationFrame"];
         }
 
         // Manual fallbacks
         if (!window.requestAnimationFrame) {
             window.requestAnimationFrame = function(callback, element) {
-                var currTime = new Date().getTime();
+                var currTime = Date.now();
                 var timeToCall = Math.max(0, 16 - (currTime - lastTime));
                 var id = window.setTimeout(function() { callback(currTime + timeToCall); },
                   timeToCall);
@@ -81,20 +81,20 @@
         
         // window.animationStartTime
         if(!window.animationStartTime) {
-            var prop;
-            for(x = 0; x < vendors.length && !window[prop]; ++x) {
-                prop = vendors[x] + 'AnimationStartTime';
-            }
-            
-            var animationStartTime;
-            if(!window[prop]) {
-                animationStartTime = function() { return +new Date; };
-            } else {
-                animationStartTime = function() { return window[prop]; };
-            }
-            if(window.__defineGetter__) {
-                window.__defineGetter__('animationStartTime', animationStartTime);
-            }
+            getter = (function() {
+                for(x = 0; x < vendors.length; ++x) {
+                    if(window[vendors[x] + "AnimationStartTime"]) {
+                        return window[vendors[x] + "AnimationStartTime"];
+                    }
+                }
+
+                return function() { return Date.now(); };
+            })();
+
+            Object.defineProperty(window, "animationStartTime", {
+                enumerable: true, configurable: false, writeable: false,
+                get: getter
+            });
         }
     }());
     
